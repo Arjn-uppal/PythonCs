c86cb2dbefe59d84b667d80dae9a4f5e7917f85e
Arjn-uppal <arjnuppal@outlook.com>
2026-03-18 00:52:48 -0400

    code complete

--- Unit 4/IntroToClasses/ICS4UcircleObject.py
+++ Unit 4/IntroToClasses/ICS4UcircleObject.py
@@ -6,57 +6,51 @@ class Circle:
         #Set the radius
         circle.radius = radius
         
+    
     #Calculates the area
     def getArea(circle):
         
         #Calculation
         circle.radius = 3.14 * circle.radius**2
+
+        return "Area of circle: " + str(circle.radius)
         
-        
+
     #Calculates the circumference
-    def getCircumference(circle):
+    def getCircumference(circle, radius):
         
+        #scale "circle.radius" value back down to radius value
+        circle.radius = radius
+
         #Calculation
         circle.radius = 2 * 3.14 * circle.radius
         
-        
-    def __str__(circle):
-        return "Area of circle: " + str(circle.radius) + "\nCircumference of circle: " + str(circle.radius) + "\n"
+        return "\nCircumference of circle: " + str(circle.radius) + "\n"
 
 
 def main():
     
-    #Creation of  circle objects
+    #Creation of circle objects
     circleObject1 = Circle(5)
     circleObject2 = Circle(6)
     circleObject3 = Circle(10)
     circleObject4 = Circle(17)
     
-    #Use area and circumference methods
-    circleObject1.getArea()
-    circleObject1.getCircumference()
+    #Use area and circumference methods and display values
+    print(circleObject1.getArea())
+    print(circleObject1.getCircumference(5))
+    print()
 
-    circleObject2.getArea()
-    circleObject2.getCircumference()
+    print(circleObject2.getArea())
+    print(circleObject2.getCircumference(6))
+    print()
 
-    circleObject3.getArea()
-    circleObject3.getCircumference()
+    print(circleObject3.getArea())
+    print(circleObject3.getCircumference(10))
+    print()
 
-    circleObject4.getArea()
-    circleObject4.getCircumference()
-    
-    #display results
-    print(circleObject1)
-    print()
-    
-    print(circleObject2)
-    print()
-    
-    print(circleObject3)
-    print()
-    
-    print(circleObject4)
-    print()
+    print(circleObject4.getArea())
+    print(circleObject4.getCircumference(17))
 
 
 
